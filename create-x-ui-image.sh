26b060ebc2d9269ebcdfea00f599e4399dbeae80
cyrus <mdoaie@yahoo.co.uk>
2022-11-27 12:02:18 +0000

    fix image creation

--- create-x-ui-image.sh
+++ create-x-ui-image.sh
@@ -9,11 +9,12 @@ cd ..
 
 echo "Image Created."
 
-docker run --network=host \
-  -v "$PWD/db/:/etc/x-ui:rw" \
-  -v "$PWD/cert/:/root/cert:rw" \
+docker run \
+  -v "$PWD/db/:/etc/x-ui" \
+  -v "$PWD/cert/:/root/cert" \
   -p 54321:54321 \
-  --name x-ui --restart=unless-stopped \
+  --name x-ui \
+  --restart unless-stopped \
   -it local/xui:latest
 
 echo "Image Created."
