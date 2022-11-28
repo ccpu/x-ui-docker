56df1c83ba8cfc2b8510c8adcd82c0070ce028ea
cyrus <mdoaie@yahoo.co.uk>
2022-11-28 13:26:12 +0000

    comment update

--- create-x-ui-image.sh
+++ create-x-ui-image.sh
@@ -4,7 +4,7 @@ git clone https://github.com/ccpu/x-ui.git
 
 # https://github.com/moby/moby/issues/2259
 # Docker image with none root user cant access volumes
-# Hance giving access to user created in image using chown
+# Hence giving access to user created in the image using chown
 mkdir db
 mkdir cert
 chown -R 4001 /home/mo/db/
